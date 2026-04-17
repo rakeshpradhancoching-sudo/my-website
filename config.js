1f8adeaf6abad9c0aa888b8a400258816fa3a53e
rakeshpradhancoching-sudo <rakeshpradhancoching@gmail.com>
2026-04-17 09:18:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,16 @@
-// config.js
+// Students Book - Final Firebase Config
 const firebaseConfig = {
-    apiKey: "YOUR_ASLI_API_KEY_YAHAN_DALEIN",
-    authDomain: "your-project.firebaseapp.com",
-    projectId: "your-project-id",
-    storageBucket: "your-project.appspot.com",
-    messagingSenderId: "123456789",
-    appId: "1:123456789:web:abcdef"
+  apiKey: "AIzaSyBuRE_hlI7FhxqL_JvHy9TbQ6yMq70jDgI",
+  authDomain: "students-book-d5e37.firebaseapp.com",
+  projectId: "students-book-d5e37",
+  storageBucket: "students-book-d5e37.firebasestorage.app",
+  messagingSenderId: "75848269402",
+  appId: "1:75848269402:web:3b9d3eee4969ff48342263"
 };
 
-// Ise initialize karein
-firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
 const auth = firebase.auth();
 const db = firebase.firestore();
-
